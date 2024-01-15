0887203f1ea42c7245ff843ba7d02443db6ba59c
Georgina Carlisle <georgina.carlisle@gmail.com>
2024-01-15 10:06:40 +0000

    FEAT: Update ESLint configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal),
-        React.createElement("h2", {}, props.breed),
-    ]);
-}
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Hammy",
-                animal: "Hamster",
-                breed: "Golden Hamster"
-            }),
-            React.createElement(Pet, {
-                name: "Cuddles",
-                animal: "Hamster",
-                breed: "Golden Hamster"
-            }),
-            React.createElement(Pet, {
-                name: "Buster",
-                animal: "Dog",
-                breed: "Collie cross"
-            }),
-        ]
-    );
+  return (
+    <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App />);
